ece19bb2b11d7a09d4422c739878bb948a2c1ae1
Willem Jacobs <wamj58@gmail.com>
2021-08-03 22:56:58 -0500

    added weather api call out

--- server.js
+++ server.js
@@ -2,46 +2,47 @@
 
 const express = require("express");
 const app = express();
+const axios = require("axios");
 const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
 const PORT = process.env.PORT;
 
-const weatherData = require("./data/weather.json");
-
 class Forecast {
-  constructor(description, date) {
-    this.description = description;
-    this.date = date;
+  constructor(info) {
+    this.description = `Low of ${info.low_temp}, high of ${info.max_temp} with ${info.weather.description}`;
+    this.date = info.datetime;
+    this.icon = info.weather.icon;
   }
 }
 
-app.get("/weather", (req, res) => {
-  const cityName = req.query.city;
-  if (cityName) {
-    const weatherArray = [];
-    const cityWeather = weatherData.find(
-      (city) => city.city_name.toLowerCase() === cityName.toLowerCase()
-    );
-    if (cityWeather) {
-      cityWeather.data.map((info) => {
-        weatherArray.push(
-          new Forecast(
-            `Low of ${info.low_temp}, high of ${info.max_temp} with ${info.weather.description}`,
-            info.datetime
-          )
-        );
-      });
-      res.send(weatherArray);
-    } else {
-      res.status(500).send("Something went wrong. No weather data available");
+app.get("/weather", getWeatherData);
+
+async function getWeatherData(req, res) {
+  const cityLat = req.query.lat;
+  const cityLon = req.query.lon;
+  const weatherArray = [];
+  if (cityLat || cityLon) {
+    try {
+      let results = await axios.get(
+        `https://api.weatherbit.io/v2.0/forecast/daily?lat=${cityLat}&lon=${cityLon}&units=I&days=5&key=${process.env.WEATHER_API_KEY}`
+      );
+      const result = results.data.data;
+      if (result) {
+        result.map((info) => {
+          weatherArray.push(new Forecast(info));
+        });
+        res.send(weatherArray);
+      } else {
+        res.status(500).send("Something went wrong. No weather data available");
+      }
+    } catch (error) {
+      res.status(500).send(`An error happen: ${error.message}`);
     }
   } else {
-    if (!cityName) {
-      res.status(500).send("Someting went wrong. Invalid city");
-    }
+    res.status(500).send("Someting went wrong. Invalid city");
   }
-});
+}
 
 app.get("/*", (req, res) => {
   res.status(404).send("Page not found");
