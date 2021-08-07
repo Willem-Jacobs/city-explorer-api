a2322d7e8653850c53ba3b37c6776cefb4aa5a5c
Willem Jacobs <wamj58@gmail.com>
2021-08-07 14:04:07 -0500

    movies moved

---
+++ movies.js
@@ -0,0 +1,42 @@
+const axios = require("axios");
+
+class Movie {
+  constructor(movie) {
+    this.title = movie.original_title;
+    this.overview = movie.overview;
+    this.average_votes = movie.vote_average;
+    this.total_votes = movie.vote_count;
+    this.image_url = movie.poster_path
+      ? `http://image.tmdb.org/t/p/w500${movie.poster_path}`
+      : "";
+    this.popularity = movie.popularity;
+    this.released_on = movie.release_date;
+  }
+}
+
+async function getMovieData(req, res) {
+  const search = req.query.search;
+  const resultArray = [];
+  if (search) {
+    try {
+      let results = await axios.get(
+        `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.MOVIE_API_KEY}&query=${search}`
+      );
+      const result = results.data.results;
+      if (result) {
+        result.map((info) => {
+          resultArray.push(new Movie(info));
+        });
+        res.send(resultArray);
+      } else {
+        res.status(500).send("Something went wrong with searching for movies");
+      }
+    } catch (error) {
+      res.status(500).send(`An error happen: ${error.message}`);
+    }
+  } else {
+    res.status(500).send("Someting went wrong with the location value");
+  }
+}
+
+module.exports = getMovieData;
--- server.js
+++ server.js
@@ -2,54 +2,15 @@
 
 const express = require("express");
 const app = express();
-const axios = require("axios");
 const cors = require("cors");
 app.use(cors());
 require("dotenv").config();
 const PORT = process.env.PORT;
 
 const getWeatherData = require("./weather");
-
-class Movie {
-  constructor(movie) {
-    this.title = movie.original_title;
-    this.overview = movie.overview;
-    this.average_votes = movie.vote_average;
-    this.total_votes = movie.vote_count;
-    this.image_url = movie.poster_path
-      ? `http://image.tmdb.org/t/p/w500${movie.poster_path}`
-      : "";
-    this.popularity = movie.popularity;
-    this.released_on = movie.release_date;
-  }
-}
+const getMovieData = require("./movies");
 
 app.get("/movies", getMovieData);
-
-async function getMovieData(req, res) {
-  const search = req.query.search;
-  const resultArray = [];
-  if (search) {
-    try {
-      let results = await axios.get(
-        `https://api.themoviedb.org/3/search/movie?&api_key=${process.env.MOVIE_API_KEY}&query=${search}`
-      );
-      const result = results.data.results;
-      if (result) {
-        result.map((info) => {
-          resultArray.push(new Movie(info));
-        });
-        res.send(resultArray);
-      } else {
-        res.status(500).send("Something went wrong with searching for movies");
-      }
-    } catch (error) {
-      res.status(500).send(`An error happen: ${error.message}`);
-    }
-  } else {
-    res.status(500).send("Someting went wrong with the location value");
-  }
-}
 
 app.get("/weather", getWeatherData);
 
